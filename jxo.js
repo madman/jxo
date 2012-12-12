23a58c7fb6cde253ab6b17d7704ab11f3124964d
юр <@madman-pc>
2012-12-12 21:56:02 +0200

    created jxo javascript file

---
+++ jxo.js
@@ -0,0 +1,14 @@
+/* JXO - v0.1 - 2012-12-12
+* http://devcomm.org.ua/jxo
+* Copyright (c) 2012 Devcomm */
+
+
+(function ($) {
+  $.fn.jxo = function () {
+    return this.each(function () {
+      // TODO: code here
+    });
+  };
+})(jQuery);
+
+
